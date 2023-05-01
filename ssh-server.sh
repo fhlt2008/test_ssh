38c082a8605019540fcf410842d6a52540257c08
fhlt2008 <fhlt2008@qq.com>
2023-05-01 11:57:45 +0800

    Update ssh-server.sh

--- ssh-server.sh
+++ ssh-server.sh
@@ -10,7 +10,7 @@ if [[ -z "$USER_PASS" ]]; then
   exit 3
 fi
  
-echo "### Install ngrok 20230424###"
+echo "### Install ngrok 20230501###"
  
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
