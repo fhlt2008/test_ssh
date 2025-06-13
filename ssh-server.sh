5568adc32a5ffe20a0ff5be5c60be1d7b952b11c
fhlt2008 <fhlt2008@qq.com>
2025-06-13 08:20:08 +0800

    Fri Jun 13 08:20:08 CST 2025

--- ssh-server.sh
+++ ssh-server.sh
@@ -10,7 +10,7 @@ if [[ -z "$USER_PASS" ]]; then
   exit 3
 fi
  
-echo "### Install ngrok Sun Jun  8 08:54:59 CST 2025 ###
+echo "### Install ngrok Fri Jun 13 08:20:08 CST 2025 ###
  
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
